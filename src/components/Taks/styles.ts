ebda3272119e7246a20d86d6469348b5385ac78e
Lucas Rocha <55117054+lucasrrocha@users.noreply.github.com>
2023-07-14 19:12:40 -0300

    refactor(todo): remove last theme

--- src/components/Taks/styles.ts
+++ src/components/Taks/styles.ts
@@ -1,30 +1,8 @@
-import { StyleSheet } from "react-native";
 import { css, styled } from "styled-components/native";
-import { AppColors } from "../../assets/styles/Colors";
 
 type Props = {
   value: boolean;
 }
-
-export const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    backgroundColor: AppColors.gray500,
-    borderRadius: 8,
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
-    padding: 12,
-    borderWidth: 1,
-    borderColor: AppColors.gray400
-  },
-  task: {
-    flex: 1,
-    fontSize: 14,
-    fontWeight: '400',
-    marginLeft: 8,
-  }
-});
 
 export const Container = styled.View`
   width: 100%;
